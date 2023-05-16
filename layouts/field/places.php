9ab44dbe61c42634585d93225d79b339c56db6c7
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2023-05-16 16:22:31 +0300

    Finish method form

--- layouts/field/places.php
+++ layouts/field/places.php
@@ -10,7 +10,9 @@
  */
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
 
 \defined('_JEXEC') or die;
 
@@ -54,14 +56,42 @@ extract($displayData);
 // Load assets
 if (!empty($map_key))
 {
+	/** @var \Joomla\CMS\Document\Document $document */
+	$document = Factory::getApplication()->getDocument();
+
 	/** @var \Joomla\CMS\WebAsset\WebAssetManager $assets */
-	$assets         = Factory::getApplication()->getDocument()->getWebAssetManager();
+	$assets         = $document->getWebAssetManager();
 	$assetsRegistry = $assets->getRegistry();
 
 	$assetsRegistry->addExtensionRegistryFile('plg_radicalmart_shipping_apiship');
 	$assets->registerAndUseScript('plg_radicalmart_shipping_apiship.map',
-		'//api-maps.yandex.ru/2.1/?lang=ru-RU&apikey=' . $map_key, [], ['defer' => true, 'type' => 'module'])
+		'//api-maps.yandex.ru/2.1/?lang=ru-RU&apikey=' . $map_key, [])
 		->usePreset('plg_radicalmart_shipping_apiship.fields.places');
+
+	$markerSize = 32;
+	$marker     = HTMLHelper::image('plg_radicalmart_shipping_apiship/marker.php', '',
+			'', true, true) . '?size=' . $markerSize;
+
+	$jsOptions = [
+		'iconImageHref'       => $marker . '&color=fa1f0c',
+		'iconImageSize'       => [$markerSize, $markerSize],
+		'iconImageOffset'     => [($markerSize / 2) * -1, $markerSize * -1],
+		'iconActiveImageHref' => $marker . '&color=147247',
+		'markers'             => false,
+		'hiddenLabel'         => $hiddenLabel
+	];
+
+	if (!empty($value))
+	{
+		foreach ($value as $key => $item)
+		{
+			$jsOptions['markers'][$key] = [(float) $item['latitude'], (float) $item['longitude']];
+			$jsOptions['center']        = false;
+			$jsOptions['zoom']          = false;
+		}
+	}
+
+	$document->addScriptOptions($id, $jsOptions);
 }
 ?>
 <div>
@@ -70,6 +100,56 @@ if (!empty($map_key))
 			<?php echo Text::_($map_error); ?>
 		</div>
 	<?php else: ?>
+		<div id="<?php echo $id; ?>" radicalmart-shipping-apiship-field-places="container" data-mode="edit"
+			 class="card">
+			<div radicalmart-shipping-apiship-field-places="template" style="display: none">
+				<?php echo LayoutHelper::render('plugins.radicalmart_shipping.apiship.field.places.row',
+					['id' => $id, 'name' => $name]); ?>
+			</div>
+
+			<div class="row g-1">
+				<div class="col-lg-8 col-xl-7">
+					<div id="<?php echo $id . '_map'; ?>" radicalmart-shipping-apiship-field-places="map"
+						 class="bg-light">
+						<span class="btn btn-success" radicalmart-shipping-apiship-field-places="action-add">
+							<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_ACTION_ADD'); ?>
+						</span>
+						<span class="btn btn-danger" radicalmart-shipping-apiship-field-places="action-cancel_add">
+							<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_ACTION_CANCEL_ADD'); ?>
+						</span>
+						<span class="btn btn-primary" radicalmart-shipping-apiship-field-places="action-show_all">
+							<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_ACTION_SHOW_ALL'); ?>
+						</span>
+					</div>
+				</div>
+				<div class="fields-overlay col-lg-4 col-xl-5">
+					<div class="text-center p-2">
+						<div radicalmart-shipping-apiship-field-places="message-no_select_items" class="text-warning"
+							 style="display: none">
+							<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_MESSAGE_NO_SELECT_ITEMS'); ?>
+						</div>
+						<div radicalmart-shipping-apiship-field-places="message-no_items" class="text-error"
+							 style="display: none">
+							<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_MESSAGE_NO_ITEMS'); ?>
+						</div>
+						<div radicalmart-shipping-apiship-field-places="message-add" class="text-success"
+							 style="display: none">
+							<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_MESSAGE_ADD'); ?>
+						</div>
+					</div>
+					<div radicalmart-shipping-apiship-field-places="form">
+						<?php if (!empty($value))
+						{
+							foreach ($value as $item)
+							{
+								echo LayoutHelper::render('plugins.radicalmart_shipping.apiship.field.places.row',
+									['id' => $id, 'name' => $name, 'value' => $item]);
+							}
+						} ?>
+					</div>
+				</div>
+			</div>
+		</div>
 	<?php endif; ?>
 </div>
 
---
+++ layouts/field/places/row.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ * @package    RadicalMart Shipping ApiShip Plugin
+ * @subpackage  plg_radicalmart_shipping_apiship
+ * @version     __DEPLOY_VERSION__
+ * @author      Delo Design - delo-design.ru
+ * @copyright   Copyright (c) 2023 Delo Design. All rights reserved.
+ * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link        https://delo-design.ru/
+ */
+
+use Joomla\CMS\Language\Text;
+
+\defined('_JEXEC') or die;
+
+extract($displayData);
+
+/**
+ * Layout variables
+ * -----------------
+ *
+ * @var  string $id    DOM id of the field.
+ * @var  string $name  Name of the input field.
+ * @var  array  $value Field value.
+ *
+ */
+
+$replaceAttribute = '';
+
+if (empty($value))
+{
+	$value            = ['title' => '', 'address' => '', 'key' => 'REPLACE_TO_KEY', 'latitude' => '', 'longitude' => ''];
+	$replaceAttribute = 'data-replace_';
+}
+
+$key  = $value['key'];
+$id   .= '_' . $key;
+$name .= '[' . $key . ']';
+?>
+<div radicalmart-shipping-apiship-field-places="item" data-key="<?php echo $key; ?>" data-active="0"
+	 class="options-form form-vertical" style="display: none">
+	<div class="text-end">
+		<a href="javascript:void(0);" radicalmart-shipping-apiship-field-places="action-close"
+		   class="icon-cancel text-danger"
+		   title="<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_ACTION_CLOSE'); ?>"></a>
+	</div>
+
+	<div class="control-group">
+		<label for="<?php echo $id . '_title'; ?>" class="control-label">
+			<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_ROW_TITLE'); ?>
+		</label>
+		<div class="controls">
+			<input type="text" radicalmart-shipping-apiship-field-places="title" class="form-control"
+			       <?php echo $replaceAttribute; ?>id="<?php echo $id . '_title'; ?>"
+			       <?php echo $replaceAttribute; ?>name="<?php echo $name . '[title]'; ?>"
+				   value="<?php echo $value['title']; ?>">
+		</div>
+	</div>
+	<div class="control-group">
+		<label for="<?php echo $id . '_address'; ?>" class="control-label">
+			<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_ROW_ADDRESS'); ?>
+		</label>
+		<div class="controls">
+			<input type="text" radicalmart-shipping-apiship-field-places="address" class="form-control"
+			       <?php echo $replaceAttribute; ?>id="<?php echo $id . '_address'; ?>"
+			       <?php echo $replaceAttribute; ?>name="<?php echo $name . '[address]'; ?>"
+				   value="<?php echo $value['address']; ?>">
+		</div>
+	</div>
+	<div>
+		<span class="btn btn-sm btn-outline-danger" radicalmart-shipping-apiship-field-places="action-delete">
+			<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_ACTION_DELETE'); ?>
+		</span>
+		<span class="btn btn-sm btn-outline-secondary" radicalmart-shipping-apiship-field-places="action-set_address">
+			<?php echo Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PLACES_ACTION_SET_ADDRESS'); ?>
+		</span>
+	</div>
+	<input type="hidden" radicalmart-shipping-apiship-field-places="key"
+	       <?php echo $replaceAttribute; ?>id="<?php echo $id . '_key'; ?>"
+	       <?php echo $replaceAttribute; ?>name="<?php echo $name . '[key]'; ?>"
+		   value="<?php echo $value['key']; ?>">
+	<input type="hidden" radicalmart-shipping-apiship-field-places="latitude"
+	       <?php echo $replaceAttribute; ?>id="<?php echo $id . '_latitude'; ?>"
+	       <?php echo $replaceAttribute; ?>name="<?php echo $name . '[latitude]'; ?>"
+		   value="<?php echo $value['latitude']; ?>">
+	<input type="hidden" radicalmart-shipping-apiship-field-places="longitude"
+	       <?php echo $replaceAttribute; ?>id="<?php echo $id . '_longitude '; ?>"
+	       <?php echo $replaceAttribute; ?>name="<?php echo $name . '[longitude]'; ?>"
+		   value="<?php echo $value['longitude']; ?>">
+</div>
