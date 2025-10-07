8737dc9de999847fdde60f95d905a2342c7d6ba6
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2025-10-07 13:10:53 +0300

    Cleanup

---
+++ src/Traits/RetailCRMTrait.php
@@ -0,0 +1,241 @@
+<?php
+/*
+ * @package     RadicalMart Shipping ApiShip Plugin
+ * @subpackage  plg_radicalmart_shipping_apiship
+ * @version     __DEPLOY_VERSION__
+ * @author      RadicalMart Team - radicalmart.ru
+ * @copyright   Copyright (c) 2025 RadicalMart. All rights reserved.
+ * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link        https://radicalmart.ru/
+ */
+
+namespace Joomla\Plugin\RadicalMartShipping\ApiShip\Traits;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\Plugin\RadicalMart\RetailCRM\Helper\RetailCRMHelper;
+use Joomla\Registry\Registry;
+
+trait RetailCRMTrait
+{
+	/**
+	 * Prepare RetailCRM Shipping method data.
+	 *
+	 * @param   object    $method     Shipping method data.
+	 * @param   Registry  $crmParams  RetailCRM params.
+	 * @param   array     $data       RetailCRM delivery type data.
+	 * @param   array     $services   RetailCRM delivery services data.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public function onRadicalMartRetailCRMExportShippingMethodData(object $method, Registry $crmParams, array &$data, array &$services): void
+	{
+		foreach (self::getShippingMethodParams($method->id)->get('sender') as $sender)
+		{
+			$services[] = $this->getRetailCRMDeliveryServiceData($method, $crmParams, $sender);
+		}
+	}
+
+	/**
+	 * Prepare RetailCRM order data.
+	 *
+	 * @param   object    $order      Current order object.
+	 * @param   Registry  $crmParams  RetailCRM params.
+	 * @param   array     $data       RetailCRM order data.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public function onRadicalMartRetailCRMExportOrderData(object $order, Registry $crmParams, array &$data): void
+	{
+		$this->setRetailCRMDeliveryData($order, $crmParams, $data);
+	}
+
+	/**
+	 * Method to get RetailCRM delivery service data.
+	 *
+	 * @param   object    $method     Shipping method data.
+	 * @param   Registry  $crmParams  RetailCRM params.
+	 * @param   array     $sender     Sender params.
+	 *
+	 * @return array RetailCRM delivery service data.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected function getRetailCRMDeliveryServiceData(object $method, Registry $crmParams, array $sender): array
+	{
+		$method_params = self::getShippingMethodParams($method->id);
+		$delivery_type = $method_params->get('delivery_type');
+
+		$prefix     = $crmParams->get('id_prefix');
+		$methodCode = RetailCRMHelper::getSymbolicCode($prefix, 'shipping_method', $method->id);
+
+		$suffix = implode('_', [
+			$sender['provider'],
+			'pt' . $sender['pickup_type'],
+			'dt' . $delivery_type,
+		]);
+
+		$name = Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PROVIDER_' . $sender['provider']) . ': '
+			. Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PICKUP_TYPE_' . $sender['pickup_type']) . ' > '
+			. Text::_('PLG_RADICALMART_SHIPPING_APISHIP_DELIVERY_TYPE_' . $delivery_type);
+
+		return [
+			'code'         => RetailCRMHelper::getSymbolicCode($prefix, 'shipping_method', $method->id, $suffix),
+			'name'         => $name,
+			'active'       => true,
+			'deliveryType' => $methodCode,
+		];
+	}
+
+	/**
+	 * Method to set RetailCRM delivery order data.
+	 *
+	 * @param   object    $order      Current order object.
+	 * @param   Registry  $crmParams  RetailCRM params.
+	 * @param   array     $data       RetailCRM order data.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected function setRetailCRMDeliveryData(object $order, Registry $crmParams, array &$data): void
+	{
+		$method_id = $order->shipping->id;
+		$shipping  = $order->formData['shipping'];
+
+		$params        = self::getShippingMethodParams($method_id);
+		$delivery_type = (int) $params->get('delivery_type', 2);
+		$provider      = ($delivery_type === 2) ? $shipping['point']['providerKey'] : $shipping['address']['provider'];
+
+		$senders      = $params->get('sender', []);
+		$senderParams = (isset($senders[$provider])) ? $senders[$provider] : false;
+		$pickup_type  = (isset($senderParams['pickup_type'])) ? (int) $senderParams['pickup_type'] : 1;
+
+		if (!isset($data['delivery']['address']))
+		{
+			$data['delivery']['address'] = [];
+		}
+
+		if (!isset($data['delivery']['data']))
+		{
+			$data['delivery']['data'] = [];
+		}
+
+		$data['delivery']['service'] = $this->getRetailCRMDeliveryServiceData($order->shipping, $crmParams, $senderParams);
+
+		$data['delivery']['data']['tariff']     = $shipping['tariff']['id'];
+		$data['delivery']['data']['tariffName'] = $shipping['tariff']['name'];
+
+		$data['delivery']['address']['notes'] = [];
+
+		$data['delivery']['address']['notes'][] =
+			Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PROVIDER') . ': '
+			. Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PROVIDER_' . $provider);
+
+		$data['delivery']['address']['notes'][] =
+			Text::_('PLG_RADICALMART_SHIPPING_APISHIP_SHIPPING_TARIFF') . ': '
+			. $shipping['tariff']['name'];
+		$data['delivery']['address']['notes'][] = '';
+
+		if (!empty($shipping['api_order']))
+		{
+			if (!empty($shipping['api_order']['id']))
+			{
+				$data['delivery']['data']['externalId'] = $shipping['api_order']['id'];
+
+				$data['delivery']['address']['notes'][] =
+					Text::_('PLG_RADICALMART_SHIPPING_APISHIP_API_ORDER_ID') . ': '
+					. $shipping['api_order']['id'];
+			}
+
+			if (!empty($shipping['api_order']['status']))
+			{
+				$data['delivery']['address']['notes'][] =
+					Text::_('PLG_RADICALMART_SHIPPING_APISHIP_API_ORDER_STATUS') . ': '
+					. $shipping['api_order']['status'];
+			}
+
+			$data['delivery']['address']['notes'][] = '';
+		}
+
+		$data['delivery']['address']['notes'][] =
+			Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PICKUP_TYPE') . ': '
+			. Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PICKUP_TYPE_' . $senderParams['pickup_type']);
+
+		if ($pickup_type === 1)
+		{
+			$data['delivery']['address']['notes'][] =
+				Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PICKUP_ADDRESS') . ': '
+				. $senderParams['address'];
+		}
+		else
+		{
+			$data['delivery']['address']['notes'][] =
+				Text::_('PLG_RADICALMART_SHIPPING_APISHIP_PICKUP_POINT') . ': '
+				. $senderParams['point'];
+
+			$data['delivery']['data']['shipmentpointId'] = $senderParams['point'];
+		}
+		$data['delivery']['address']['notes'][] = '';
+
+		$data['delivery']['address']['notes'][] =
+			Text::_('PLG_RADICALMART_SHIPPING_APISHIP_DELIVERY_TYPE') . ' : '
+			. Text::_('PLG_RADICALMART_SHIPPING_APISHIP_DELIVERY_TYPE_' . $delivery_type);
+
+		if ($delivery_type === 2)
+		{
+			$data['delivery']['address']['name']       = $shipping['point']['display'];
+			$data['delivery']['address']['countryIso'] = $shipping['point']['countryCode'];
+			$data['delivery']['address']['text']       = $shipping['point']['countryCode'] . ' ' . $shipping['point']['address'];
+
+			$data['delivery']['data']['pickuppointId']                  = $shipping['point']['id'];
+			$data['delivery']['data']['pickuppointAddress']             = $shipping['point']['address'];
+			$data['delivery']['data']['pickuppointName']                = $shipping['point']['title'];
+			$data['delivery']['data']['pickuppointCoordinateLatitude']  = $shipping['point']['latitude'];
+			$data['delivery']['data']['pickuppointCoordinateLongitude'] = $shipping['point']['longitude'];
+
+			$data['delivery']['address']['notes'][] =
+				Text::_('PLG_RADICALMART_SHIPPING_APISHIP_POINT') . ': ' . $shipping['point']['address'];
+		}
+		else
+		{
+			foreach ([
+				         'index'    => 'zip',
+				         'country'  => 'country',
+				         'region'   => 'region',
+				         'city'     => 'city',
+				         'street'   => 'street',
+				         'building' => 'house',
+				         'flat'     => 'apartment',
+				         'floor'    => 'floor',
+				         'block'    => 'entrance',
+				         'housing'  => 'building',
+				         'name'     => 'display',
+				         'text'     => 'string'
+
+			         ] as $dest => $src
+			)
+			{
+				if (empty($shipping['address'][$src]))
+				{
+					continue;
+				}
+				$data['delivery']['address'][$dest] = $shipping['address'][$src];
+			}
+		}
+		$data['delivery']['address']['notes'][] = '';
+
+		if (!empty($shipping['tracking_number']))
+		{
+			$data['delivery']['address']['notes'][] =
+				Text::_('PLG_RADICALMART_SHIPPING_APISHIP_SHIPPING_TRACKING_NUMBER') . ': ' . $shipping['tracking_number'];
+		}
+
+		if (!empty($shipping['tracking_url']))
+		{
+			$data['delivery']['address']['notes'][] =
+				Text::_('PLG_RADICALMART_SHIPPING_APISHIP_SHIPPING_TRACKING_URL') . ': ' . $shipping['tracking_url'];
+		}
+
+		$data['delivery']['address']['notes'] = implode(PHP_EOL, $data['delivery']['address']['notes']);
+	}
+}
